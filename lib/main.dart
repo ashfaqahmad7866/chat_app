aefa9fe3ba4b1fe577326a780175e7d5a33398c3
ashfaqahmad7866 <ashfaq25322@gmail.com>
2023-09-11 16:09:14 +0500

    flutter chat app initializing

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatApp',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
+      ),
+      home: const Text('data'),
+    );
+  }
+}
